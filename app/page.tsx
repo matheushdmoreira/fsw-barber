5fefaf96652b8caee9b067ff6fe04a4e1955fa9c
Matheus Moreira <matheustjp111@gmail.com>
2024-08-06 11:30:59 -0300

    feat: add booking item

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,9 @@ import Image from "next/image"
 import { Header } from "./_components/header"
 import { Button } from "./_components/ui/button"
 import { Input } from "./_components/ui/input"
+import { Card, CardContent } from "./_components/ui/card"
+import { Badge } from "./_components/ui/badge"
+import { Avatar, AvatarImage } from "./_components/ui/avatar"
 
 export default function Home() {
   return (
@@ -14,6 +17,7 @@ export default function Home() {
         <h2 className="text-xl font-bold">Olá, Matheus</h2>
         <p>Sexta-feira, 2 de Agosto</p>
 
+        {/* BUSCA */}
         <div className="mt-6 flex items-center gap-2">
           <Input placeholder="Faça sua busca..." />
 
@@ -22,6 +26,7 @@ export default function Home() {
           </Button>
         </div>
 
+        {/* IMAGEM */}
         <div className="relative mt-6 h-[150px] w-full">
           <Image
             alt="Agende nos melhores com FSW Barber"
@@ -30,6 +35,33 @@ export default function Home() {
             className="rounded-xl object-cover"
           />
         </div>
+
+        {/* AGENDAMENTO */}
+        <Card className="mt-6">
+          <CardContent className="flex justify-between p-0">
+            {/* LEFT */}
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit">Confirmado</Badge>
+
+              <h3 className="font-bold">Corte de Cabelo</h3>
+
+              <div className="flex items-center gap-2">
+                <Avatar className="size-6">
+                  <AvatarImage src="https://github.com/matheushdmoreira.png" />
+                </Avatar>
+
+                <p className="text-sm">Barbearia FSW</p>
+              </div>
+            </div>
+
+            {/* RIGHT */}
+            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+              <p className="text-sm">Agosto</p>
+              <p className="text-2xl">06</p>
+              <p className="text-sm">10:45</p>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   )
